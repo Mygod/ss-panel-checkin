52d629ba4b20d9ec1e37ed351ac5f41d5fe83dfa
Mygod <mygodstudio@gmail.com>
2015-09-12 22:07:10 +0800

    Bump version number

--- ss-panel-checkin/Properties/AssemblyInfo.cs
+++ ss-panel-checkin/Properties/AssemblyInfo.cs
@@ -41,5 +41,5 @@ using System.Windows;
 // 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.12.359")]
-[assembly: AssemblyFileVersion("1.4.12.359")]
+[assembly: AssemblyVersion("1.4.13.360")]
+[assembly: AssemblyFileVersion("1.4.13.360")]
